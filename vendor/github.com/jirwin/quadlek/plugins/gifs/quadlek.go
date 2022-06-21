36ec9b9430cb82eabe4067a8eb57e4d9658b0885
Justin Gallardo <me@jirw.in>
2022-06-20 23:26:14 -0700

    Use new std gifs lib (#46)

--- vendor/github.com/jirwin/quadlek/plugins/gifs/quadlek.go
+++ vendor/github.com/jirwin/quadlek/plugins/gifs/quadlek.go
@@ -2,13 +2,11 @@ package gifs
 
 import (
 	"context"
-	"go.uber.org/zap"
-
 	"fmt"
-
+	"net/url"
 	"strings"
 
-	"net/url"
+	"go.uber.org/zap"
 
 	"github.com/jirwin/quadlek/quadlek"
 )
@@ -23,7 +21,7 @@ func gifCommand(ctx context.Context, cmdChannel <-chan *quadlek.CommandMsg) {
 	for {
 		select {
 		case cmdMsg := <-cmdChannel:
-			text := cmdMsg.Command.Text
+			text := strings.TrimPrefix(cmdMsg.Command.Text, "url:")
 			if text != "" {
 				var gifUrl string
 				var err error
@@ -110,6 +108,35 @@ func gifSaveCommand(ctx context.Context, cmdChannel <-chan *quadlek.CommandMsg) 
 	}
 }
 
+func gifListCommand(ctx context.Context, cmdChannel <-chan *quadlek.CommandMsg) {
+	for {
+		select {
+		case cmdMsg := <-cmdChannel:
+			sb := &strings.Builder{}
+			err := cmdMsg.Store.ForEach(func(key string, value []byte) error {
+				if strings.HasPrefix(key, "url:") {
+					return nil
+				}
+				_, err := sb.WriteString(fmt.Sprintf("%s => %s\n", key, string(value)))
+				if err != nil {
+					return err
+				}
+				return nil
+			})
+			if err != nil {
+				continue
+			}
+			cmdMsg.Command.Reply() <- &quadlek.CommandResp{
+				Text:      sb.String(),
+				InChannel: false,
+			}
+
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
 func gifReaction(ctx context.Context, reactionChannel <-chan *quadlek.ReactionHookMsg) {
 	for {
 		select {
@@ -158,6 +185,7 @@ func Register(apiKey string) quadlek.Plugin {
 		[]quadlek.Command{
 			quadlek.MakeCommand("g", gifCommand),
 			quadlek.MakeCommand("gsave", gifSaveCommand),
+			quadlek.MakeCommand("glist", gifListCommand),
 		},
 		nil,
 		[]quadlek.ReactionHook{
